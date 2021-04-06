a21aa85b0537bec68417fcc65ebcce5d0eb00adf
akashudl <78961715+akashudl@users.noreply.github.com>
2021-04-06 18:09:17 +0530

    Update CommandsAvailable.jsx

--- Client/src/Components/CommandsAvailable.jsx
+++ Client/src/Components/CommandsAvailable.jsx
@@ -19,7 +19,7 @@ export default class CommandsAvailable extends Component {
       componentDidMount() 
       { 
        
-       axios.get("http://localhost:3006/api/v1/commanddetails").then(response=>{
+       axios.get("http://172.16.7.249:3006/api/v1/commanddetails").then(response=>{
          this.setState({userInfo:response.data.data.user})
          console.log(response);
         });
